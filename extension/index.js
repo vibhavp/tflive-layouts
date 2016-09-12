ddc64a5acd162491b4eb5c27d2ff9e2fe35b0fc7
Vibhav Pant <vibhavp@gmail.com>
2016-09-12 16:05:58 +0530

    finish main overlay

--- extension/index.js
+++ extension/index.js
@@ -2,28 +2,32 @@
 const Twitter = require('twitter');
 
 module.exports = function (nodecg) {
-	// if (!nodecg.bundleConfig) {
-	// 	nodecg.log.error('no config found. Twitter disabled');
-	// 	return;
-	// }
+	let client;
 
-	// if (typeof nodecg.bundleConfig.twitter === 'undefined') {
-	// 	nodecg.log.error('no twitter config found. Twitter disabled');
-	// 	return;
-	// }
+	if (!nodecg.bundleConfig) {
+		nodecg.log.error('no config found. Twitter disabled');
+	} else if (typeof nodecg.bundleConfig.twitter === 'undefined') {
+		nodecg.log.error('no twitter config found. Twitter disabled');
+	} else {
+		client = new Twitter({
+			consumerKey: nodecg.bundleConfig.twitter.consumer_key,
+			consumerSecret: nodecg.bundleConfig.twitter.consumer_secret,
+			accessTokenKey: nodecg.bundleConfig.twitter.access_token_key,
+			accessTokenSecret: nodecg.bundleConfig.twitter.access_token_secret
+		});
+	}
 
-	// const client = new Twitter({
-	// 	consumerKey: nodecg.bundleConfig.twitter.consumer_key,
-	// 	consumerSecret: nodecg.bundleConfig.twitter.consumer_secret,
-	// 	accessTokenKey: nodecg.bundleConfig.twitter.access_token_key,
-	// 	accessTokenSecret: nodecg.bundleConfig.twitter.access_token_secret
-	// });
+	nodecg.Replicant('maps', 'tflive', {defaultValue: []});
 
-	let caster1 = new nodecg.Replicant('caster1', 'tflive', {defaultValue: {}});
-	caster1.on('change', newValue => {
-		console.log(newValue);
-		const info = new nodecg.Replicant('caster1_info', 'tflive');
-		info.value = {twitter_img: null, name: newValue.name, twitter: newValue.twitter};
-	})
+	function addRole(id) {
+		const rep = new nodecg.Replicant(id, 'tflive', {defaultValue: {name: 'TBD', twitter: ''}});
+		const repInfo = new nodecg.Replicant(id + 'Info', 'tflive', {defaultValue: {name: 'TBD', twitter: ''}});
+		rep.on('change', data => {
+			repInfo.value = {twitterImg: null, name: data.name, twitter: data.twitter};
+		});
+	}
 
+	addRole('caster1');
+	addRole('caster2');
+	addRole('observer');
 };
--- graphics/index.js
+++ graphics/index.js
@@ -1,46 +1,55 @@
-(function() {
+(function () {
 	const nodecg = window.nodecg;
 	let cur_map_index = 0;
 	let show_all_maps = true;
-	let mapsDiv = document.getElementById("maps");
+	const mapsDiv = document.getElementById('maps');
 
 	function makeMapText(maps) {
-		let str = "";
+		let str = '';
+		let i;
 		for (i in maps) {
-			str += maps[i];
-			if (i != maps.length - 1) {
-				str += " · ";
+			if ({}.hasOwnProperty.call(maps, i)) {
+				str += maps[i];
+				if (i !== maps.length - 1) {
+					str += ' · ';
+				}
 			}
 		}
-		return str
+
+		return str;
 	}
 
 	function setMaps() {
 		nodecg.readReplicant('maps', 'tflive', maps => {
+			if (maps.length === 0) {
+				setTimeout(setMaps, 5000);
+				return;
+			}
+
 			if (show_all_maps) {
 				show_all_maps = false;
 				mapsDiv.innerText = makeMapText(maps);
 			} else {
-				mapsDiv.innerText = "Map #" + (cur_map_index + 1) +": "+ maps[cur_map_index++];
-				if (cur_map_index == maps.length) {
+				mapsDiv.innerText = 'Map #' + (cur_map_index + 1) + ': ' + maps[cur_map_index++];
+				if (cur_map_index === maps.length) {
 					show_all_maps = true;
 					cur_map_index = 0;
 				}
 			}
 			setTimeout(setMaps, 5000);
-		})
+		});
 	}
 
 	function changeTeam(color) {
-		return function(team) {
+		return function (team) {
 			const e = document.getElementById(color);
 
 			if (team === undefined || team === '') {
-				e.innerText = 'TBD'
+				e.innerText = 'TBD';
 			} else {
 				e.innerText = team;
 			}
-		}
+		};
 	}
 
 	nodecg.Replicant('team1', 'tflive').on('change', changeTeam('red'));
@@ -48,17 +57,21 @@
 	setMaps();
 
 	function setProfiles(id, role) {
-		nodecg.Replicant(id+'_info', 'tflive').on('change', data => {
+		nodecg.Replicant(id + 'Info', 'tflive').on('change', data => {
 			if (data === null || data === undefined || data === {}) {
 				return;
 			}
-			console.log(data);
-			$($('#'+id).children()[0]).attr('src', data.twitter_img);
-			$('#'+id).children()[1].innerText = data.name+'<br>@'+data.twitter+'<br>'+role;
-		})
+
+			$($('#' + id).children()[0]).attr('src', data.twitter_img);
+			// $('#' + id).children()
+
+			[1].innerHTML = '<b>' + data.name + '</b><br>@<i>' + data.twitter + '</i>';
+			$('#name', '#' + id)[0].innerHTML = '<b>' + data.name + '</b>';
+			$('#twitter', '#' + id)[0].innerHTML = '<i>@' + data.twitter + '</i>';
+		});
 	}
 
-	setProfiles('caster1');
-	setProfiles('caster2');
-	setProfiles('observer');
+	setProfiles('caster1', 'Caster');
+	setProfiles('caster2', 'Caster');
+	setProfiles('observer', 'Observer');
 })();
