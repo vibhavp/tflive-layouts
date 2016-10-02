c82b4ab7c7ab80b3121a4099357e43bc46f53f0f
Vibhav Pant <vibhavp@gmail.com>
2016-10-02 13:12:30 +0530

    try fixing twitter images

--- extension/roles.js
+++ extension/roles.js
@@ -1,6 +1,6 @@
-var Twitter = require('twitter');
+const Twitter = require('twitter');
 
-var Role = (function () {
+const Role = (function () {
 	function Role(name, twitter_id, twitter_img) {
 		if (twitter_img) {
 			twitter_img = twitter_img.replace("_normal", "");
@@ -13,16 +13,16 @@ var Role = (function () {
 }());
 
 module.exports = function (nodecg) {
-	const roles = new nodecg.Replicant("roles", "tflive", { defaultValue: {
-		"caster1": new Role(), "caster2": new Role(), "analyst1": new Role(),
-		"analyst2": new Role(), "camera": new Role(), "production": new Role()
+	const roles = new nodecg.Replicant('roles', 'tflive', { defaultValue: {
+		'caster1': new Role(), 'caster2': new Role(), 'analyst1': new Role(),
+		'analyst2': new Role(), 'camera': new Role(), 'production': new Role()
         } });
 
 	let client;
 	if (!nodecg.bundleConfig) {
-		nodecg.log.error("no config found. Twitter disabled");
-	} else if (typeof nodecg.bundleConfig.twitter === "undefined") {
-		nodecg.log.error("no twitter config found. Twitter disabled");
+		nodecg.log.error('no config found. Twitter disabled');
+	} else if (typeof nodecg.bundleConfig.twitter === 'undefined') {
+		nodecg.log.error('no twitter config found. Twitter disabled');
 	} else {
 		client = new Twitter({
 			consumer_key: nodecg.bundleConfig.twitter.consumer_key,
@@ -31,16 +31,16 @@ module.exports = function (nodecg) {
 			access_token_secret: nodecg.bundleConfig.twitter.access_token_secret
 		});
 	}
-	nodecg.listenFor("rolesChange", function (data) {
-		var twitter_img = "";
+
+	nodecg.listenFor('rolesChange', function (data) {
+		let twitter_img = '';
 		if (client) {
-			client.get("users/show", { screen_name: data.twitter }, function (error, profile) {
-				console.log(data);
+			client.get('users/show', {screen_name: data.twitter_id}, (error, profile) => {
 				if (error) {
-					nodecg.log.error("%s", error);
+					nodecg.log.error('%s', error);
 				}
 				else {
-					twitter_img = profile.profile_image_url_https;
+					twitter_img = profile.profile_image_url_http;
 				}
 			});
 		}
